6c526836d62ec30a25603a8f9e144ac8db162fbc
Cristina Ruiz <cristinaruiz96@yahoo.com>
2020-11-18 20:50:59 -0600

    Created Home and Employees pages

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Employees from "./pages/Employees";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Employee</h1>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/employees" component={Employees} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
